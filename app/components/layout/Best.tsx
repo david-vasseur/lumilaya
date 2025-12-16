6d35e1f4413a2bfe826cd833609fe48bb6807104
David <p5y4@laposte.net>
2025-12-16 16:00:18 +0100

    secure

--- app/components/layout/Best.tsx
+++ app/components/layout/Best.tsx
@@ -78,6 +78,7 @@ function Best({ products }: IBest) {
       id="best-seller"
       className="relative py-20 md:py-32 overflow-hidden"
     >
+        <span className="absolute bg-[#7A9B8E] w-[200%] h-full rotate-33" />
       {/* ---------------- Header ---------------- */}
       <div className="max-w-7xl mx-auto text-center mb-16 px-4">
         <h2 className="text-4xl md:text-5xl font-light text-[#2C2C2C] mb-4">
