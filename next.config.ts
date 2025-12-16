b9cc159fe8da13b4e0635850e4684fad76fc49b6
David <p5y4@laposte.net>
2025-12-16 19:02:35 +0100

    secure

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   
   images: {
-    domains: ['p16-sign.tiktokcdn.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'p16-sign.tiktokcdn.com',
+        port: '',        // pas nécessaire
+        pathname: '/**', // autorise toutes les images du domaine
+      },
+    ],
+    // qualities et formats facultatifs
+    formats: ['image/avif', 'image/webp'],
     qualities: [25, 50, 75, 100],
   },
 
