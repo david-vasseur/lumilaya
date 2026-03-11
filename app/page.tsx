394b9ce87b0eb7be69a79aad0e5ff490daef9058
David <p5y4@laposte.net>
2026-03-11 17:06:11 +0100

    test slug

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { ReviewsSection } from "./components/layout/ReviewSection";
 import BestProducts from "./components/layout/BestProducts";
 import TestSavoir from "./components/layout/TestSavoir";
 
+const dynamic = 'force-dynamic';
 const revalidate = 3600;
 
 export default async function Home() {
