c3f1cb41a2e3fe1bb34da3af68bfb15c4c73f641
David <p5y4@laposte.net>
2026-03-11 16:57:10 +0100

    test slug

--- app/components/layout/BestProducts.tsx
+++ app/components/layout/BestProducts.tsx
@@ -1,15 +1,43 @@
+import { IProduct } from '@/type/product'
 import { TopRatedProducts } from '../actions/product.action'
 import Best from './Best'
 
 async function BestProducts() {
+    let products: IProduct[] = []
 
-    const products = await TopRatedProducts();
+    if (process.env.SKIP_BUILD_STATIC_GENERATION === "true") {
+        // Mock minimal pour que le composant ne plante pas
+        products = [
+        {
+            id: 0,
+            collection: "Émotion",
+            name: "Produit test",
+            slug: "produit-test",
+            images: [],
+            variants: [],
+            description: [],
+            intro: "",
+            theme: [],
+            caracteristique: {
+            composition: "",
+            meche: "",
+            parfum: "",
+            combustion: "",
+            poids: "",
+            contenant: "",
+            fabrication: "",
+            },
+            stock: true,
+            promo: 0,
+            like: 0,
+            createdAt: new Date(),
+        },
+        ]
+    } else {
+        products = await TopRatedProducts()
+    }
 
-    return (
-        <>
-            <Best products={products} />
-        </>
-    )
+    return <Best products={products} />
 }
 
 export default BestProducts
